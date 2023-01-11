904b5f1ffdc2ad5090871406a0ccd54e1344a3ed
Kekalainen <git@kekalainen.me>
2023-01-12 01:01:30 +0200

    9.20: Add Entry types

--- src/types.ts
+++ src/types.ts
@@ -10,6 +10,59 @@ export enum Gender {
   Other = "other"
 }
 
+export const EntryTypes = [
+  "HealthCheck",
+  "Hospital",
+  "OccupationalHealthcare",
+] as const;
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+  type: typeof EntryTypes[number];
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalDischarge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: HospitalDischarge;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HealthCheckEntry
+  | HospitalEntry
+  | OccupationalHealthcareEntry;
+
 export interface Patient {
   id: string;
   name: string;
@@ -17,4 +70,5 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries?: Entry[];
 }
